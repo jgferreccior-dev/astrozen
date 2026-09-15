10dc5b1d4329ea8b62fbcb212f5f8c1233d3a89b
jgferreccior-dev <jgferreccior@gmail.com>
2026-09-15 13:47:13 -0500

    chore: configure Astro production site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://joseferreccio-digital-josefr-89-9123.vercel.app",
   fonts: [
     {
       provider: fontProviders.fontsource(),
